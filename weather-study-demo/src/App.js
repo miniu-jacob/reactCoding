5358dee83536c39e22e7afc8777b50e8155c5e8c
miniu <miniu.media@gmail.com>
2024-08-05 18:47:19 +0700

    weather web making

--- weather-study-demo/src/App.js
+++ weather-study-demo/src/App.js
@@ -3,14 +3,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import WeatherScreen from './component/WeatherScreen';
+import City from './component/City';
 
+const cities = ['Current Location', 'Hanoi', 'Ho Chi Minh', 'Dalat', 'Seoul', 'Nha Trang'];
 
 function App() {
+    const handleCityClick = (city) => {
+        console.log(`${city} 를 선택했음` )
+        
+    }
+    
     return (
-        <Container className='container'>
+        <Container className='white-board'>
             <Row className='justify-content-md-center'>
                 <Col md='auto'></Col>
                 <WeatherScreen></WeatherScreen>
+            </Row>
+            <Row>
+                <Col>
+                    <City cities={cities} onCityClick={handleCityClick} ></City>
+                </Col>
             </Row>
         </Container>
   );
--- weather-study-demo/src/component/WeatherScreen.js
+++ weather-study-demo/src/component/WeatherScreen.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const WeatherScreen = () => {
   return (
-    <div className='container'>
-        <div>
+    <div className='screen-box'>
+        <div className='screen-area'>
             <h2> Ho Chi Minh</h2>
             <h1>-5.10 oC / 22.82 oF </h1>
             <h2>CLEAR SKY</h2>
